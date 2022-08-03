82ed7197c70e321803119de68cc923a27c389a10
ANDY <pawaraniket1348@gmail.com>
2022-08-03 21:40:39 +0530

    Updates

---
+++ precache-manifest.dc1e564d339c60ac70cfd21b4f619d82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e893907c5dcc89cfa4b",
+    "url": "/React_Movies_App/static/css/main.4a5392cf.chunk.css"
+  },
+  {
+    "revision": "6e893907c5dcc89cfa4b",
+    "url": "/React_Movies_App/static/js/main.6e893907.chunk.js"
+  },
+  {
+    "revision": "87f7407a67ef306dbf94",
+    "url": "/React_Movies_App/static/js/1.87f7407a.chunk.js"
+  },
+  {
+    "revision": "ee12fe786ad3deed7a60",
+    "url": "/React_Movies_App/static/js/runtime~main.ee12fe78.js"
+  },
+  {
+    "revision": "1a250b40169963f1376412e517cce588",
+    "url": "/React_Movies_App/index.html"
+  }
+];
